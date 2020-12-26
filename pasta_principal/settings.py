c01c99391ad6a3fbba0bf45f2eec738c7ef8ce7e
Luciano Martins <zicadopv@gmail.com>
2020-12-26 20:52:13 -0300

    Consertando bug do static no heroku

--- pasta_principal/settings.py
+++ pasta_principal/settings.py
@@ -10,12 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
+ROOT_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(ROOT_DIR, "staticfiles")
+STATICFILES_DIRS = [
+    os.path.join(ROOT_DIR, "static")
+]
