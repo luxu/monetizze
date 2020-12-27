fccb61e7685f9797abd0f5f9531518752823cea9
Luciano Martins <zicadopv@gmail.com>
2020-12-27 19:45:16 -0300

    Finalizando o programa
    close #10

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from game import Megasena, JogarMegasena
 
-quantidade_de_dezenas = 5
+quantidade_de_dezenas = 6
+quantidade_de_cartela = 10
 ms = Megasena(quantidade_de_dezenas)
-jogar = JogarMegasena(ms)
-cartelas_para_jogar = 4
-for i in range(cartelas_para_jogar):
-    jogar.gerarCartelas()
-    jogar.setJogos()
-jogar.sortear()
-for nro_da_cartela, cartela in enumerate(jogar.getJogos()):
-    print(f'Cartela-{nro_da_cartela+1}: {cartela}')
-    print(f'Números Sorteados..: {jogar.resultadoDoSorteio()}')
-    contar_numeros_acertados = 0
-    for nro_acertados in jogar.resultadoDoSorteio():
-        if nro_acertados in cartela:
-            print(nro_acertados)
-            contar_numeros_acertados += 1
-    if contar_numeros_acertados > 0:
-        print(f'Acertou {contar_numeros_acertados} números da Cartela-{nro_da_cartela+1}\n{"*"*50}')
-    else:
-        print(f'Não houve acertou na Cartela-{nro_da_cartela + 1}\n{"*" * 50}')
+class_jogar = JogarMegasena(ms)
+isValid = class_jogar.validaQuantidadeDeDezenas()
+if isValid:
+    class_jogar.gerarJogos(quantidade_de_cartela)
+    class_jogar.sortear()
+    print(f'Cartela Sorteada..: {class_jogar.getResultado()}')
+    for nro_da_cartela, cartela in enumerate(class_jogar.getJogos()):
+        contar_numeros_acertados = 0
+        guardar_numeros_acertados = []
+        for nro_acertados in class_jogar.resultadoDoSorteio():
+            if nro_acertados in cartela:
+                guardar_numeros_acertados.append(nro_acertados)
+                contar_numeros_acertados += 1
+        if contar_numeros_acertados > 0:
+            print(
+                f'Acertou {contar_numeros_acertados} ({guardar_numeros_acertados}) '
+                f'número(s) da Cartela-{nro_da_cartela+1}\n{"*"*50}'
+            )
--- testes/megasena/test_class_jogarMegasena.py
+++ testes/megasena/test_class_jogarMegasena.py
@@ -1,18 +1,38 @@
 from game import Megasena, JogarMegasena
 
 
+def test_GET_dos_atributos_da_classe_jogarmegasena():
+    megasena = Megasena()
+    jogar = JogarMegasena(megasena)
+    assert jogar.getQuantidadeDeDezenas() == 0
+    assert jogar.getResultado() == []
+    assert jogar.getTotalJogos() == 0
+    assert jogar.getJogos() == []
+    assert jogar.getCartela() == []
+
+
+def test_SET_dos_atributos_da_classe_jogarmegasena():
+    megasena = Megasena()
+    jogar = JogarMegasena(megasena)
+    quantidade_de_dezenas = 6
+    numero = 4
+    assert jogar.setQuantidadeDeDezenas(quantidade_de_dezenas) is None
+    assert jogar.setResultado(numero) is None
+    assert jogar.setJogos() is None
+
+
 def test_valor_valido_para_quantidade_de_dezenas():
     quantidade_de_dezenas = 5
     ms = Megasena(quantidade_de_dezenas)
     jogar = JogarMegasena(ms)
-    assert jogar.megasena.validaQuantidadeDeDezenas() is False
+    assert jogar.validaQuantidadeDeDezenas() is False
 
 
 def test_valor_invalido_para_quantidade_de_dezenas():
     quantidade_de_dezenas = 11
     ms = Megasena(quantidade_de_dezenas)
     jogar = JogarMegasena(ms)
-    assert jogar.megasena.validaQuantidadeDeDezenas() is False
+    assert jogar.validaQuantidadeDeDezenas() is False
 
 
 def test_inserir_total_de_jogos():
@@ -30,30 +50,24 @@ def test_retornar_se_eh_um_array():
     assert jogar.getCartela() == []
 
 
-def test_se_array_tem_tamanho_de_quantidade_de_dezenas_definido():
-    quantidade_de_dezenas = 10
-    ms = Megasena(quantidade_de_dezenas)
-    jogar = JogarMegasena(ms)
-    assert jogar.validaCartela() is True
-
-
 def test_quais_elementos_do_array():
     quantidade_de_dezenas = 10
     ms = Megasena(quantidade_de_dezenas)
     jogar = JogarMegasena(ms)
-    if jogar.validaCartela() is True:
-        jogar.gerarCartelas()
-        print()
-    assert len(jogar.getCartela()) == quantidade_de_dezenas
+    isValid = jogar.validaQuantidadeDeDezenas()
+    if isValid:
+        jogar.setCartela()
+        assert len(jogar.getCartela()) == quantidade_de_dezenas
 
 
 def test_quantidade_de_jogos():
     quantidade_de_dezenas = 10
+    quantidade_de_cartela = 5
     ms = Megasena(quantidade_de_dezenas)
     jogar = JogarMegasena(ms)
-    jogar.gerarCartelas()
-    jogar.setJogos()
-    assert jogar.getTotalJogos() == 1
+    jogar._gerarCartelas(quantidade_de_dezenas)
+    jogar.gerarJogos(quantidade_de_cartela)
+    assert jogar.getTotalJogos() == quantidade_de_cartela
 
 
 def test_sorteio():
